73010943a359af3ce1d97f81691578fa4be464d0
luisfayez <meshachfayemi@gmail.com>
2022-10-14 16:34:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+/**
+ *main-lets do something hard
+ *Retuen:Alwyas 0, else error
+ **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
