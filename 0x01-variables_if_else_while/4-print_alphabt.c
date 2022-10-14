72f945f85409a7af42485ccb7be8b334dd4b139a
luisfayez <meshachfayemi@gmail.com>
2022-10-14 16:49:40 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-lets do something hard
+ *Return:Always 0, else error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
