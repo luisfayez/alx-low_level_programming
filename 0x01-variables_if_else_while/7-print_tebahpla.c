e0ef56853c33d9bed238ebb61e8a5fc41edc6842
luisfayez <meshachfayemi@gmail.com>
2022-10-14 17:27:58 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-lets get it done
+ * Return:0,else error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
