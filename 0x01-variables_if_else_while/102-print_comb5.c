f7f08d59089dfa35800a28189c24c02a6f125dd3
luisfayez <meshachfayemi@gmail.com>
2022-10-14 19:40:17 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,6 +15,11 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			putchar(' ');
+
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
 
