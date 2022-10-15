dad1f18e37e5b9c4921a579fca20a821cd038813
luisfayez <meshachfayemi@gmail.com>
2022-10-14 17:06:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-lets do aomething hard
+ *Return:Always 0, else error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
