715404c75279a2e2972f86813c299a3c0a3fe67a
luisfayez <meshachfayemi@gmail.com>
2022-10-14 17:21:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-something bigg
+ * Return:0 else not correct
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
