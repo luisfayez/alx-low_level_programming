e620c6d7365ff1cf27bcdb991b5c7e8e2f35e4c5
luisfayez <meshachfayemi@gmail.com>
2022-10-18 15:49:20 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - print lowercase alpha 10x
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
