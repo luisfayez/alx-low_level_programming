90bbe0c082856d3453d5174d2f06be8fa1e2a11b
luisfayez <meshachfayemi@gmail.com>
2022-10-20 08:34:16 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+ * File: 1-main.c
+ * Auth: luisfayezCode
+ */
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf"(Infinite loop avoided \\o/\n";
+
+	return (0);
+}
