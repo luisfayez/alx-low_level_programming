257cb7050ba0750d8b1b26c02dbb0aac4aa85be3
luisfayez <meshachfayemi@gmail.com>
2022-10-19 23:08:40 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+/*
+ * File: 101-natural.c
+ * Auth: luisfayeCode
+ */
+
+
+#include <stdi.h>
 
 /**
- * main-prints the sum of all multiples 
+ * main -list all natural n numbers blow 1024 (excluded)
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -10,9 +17,9 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0 || (i % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
-			}
+	}
 
 	printf("%d\n", sum);
 
