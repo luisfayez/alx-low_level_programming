07425f14561d5d4c9bd7fbdbabd0c82b42ee1bad
luisfayez <meshachfayemi@gmail.com>
2022-10-20 23:56:59 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,21 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void
+print_most_numbers(void);
+more_numbers(void);
+void print_square(int size);
+void print_number(int n);
+void print_triangle(int size);
+void print_line(int n);
+more_numbers(void);
+void print_diagonal(int n);
+
 
 #endif /* MAIN_H */
 
