89adc80bb8bdf5fec7bf71c7437f522de48b22ad
luisfayez <meshachfayemi@gmail.com>
2022-10-21 19:51:06 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char);
-void
-print_most_numbers(void);
-more_numbers(void);
 
-
-#endif
+#endif /* MAIN_H */
